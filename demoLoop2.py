766de31d253d8403269a107147aa9a3dd7aa9c23
Cu-is-Copper <dlawodbs12@gmail.com>
2025-04-14 16:39:03 +0900

    깃허브 연습2

--- demoLoop2.py
+++ demoLoop2.py
@@ -2,4 +2,18 @@
 
 for item in [1,2,3,]:
     print(item)
-    
+
+# 수열함수
+print(list(range(1,11)))
+print(list(range(2000, 2026)))
+print(list(range(1,32)))
+
+# 리스트 내장(함축)
+lst = list(range(1,11))
+print(lst)
+print([i**2 for i in lst if i>5])
+tp = ("apple", "kiwi")
+print([len(i) for i in tp])
+d = {100:"apple", 200:"kiwi"}
+print([v.upper() for v in d.values()])
+
