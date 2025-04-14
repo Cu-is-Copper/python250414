a6f68dc207c6fcf9eddc46f31a624c8f3f98eee4
Cu-is-Copper <dlawodbs12@gmail.com>
2025-04-14 16:44:36 +0900

    월요일 수업 완료

--- demoLoop2.py
+++ demoLoop2.py
@@ -17,3 +17,17 @@ print([len(i) for i in tp])
 d = {100:"apple", 200:"kiwi"}
 print([v.upper() for v in d.values()])
 
+print("---필터링함수---")
+def getBiggerThan20(i):
+    return i >20
+
+lst = [10,25,30]
+itemL = filter(getBiggerThan20, lst)
+for item in itemL:
+    print(item)
+
+print("---람다함수---")
+lst = [10,25,30]
+itemL = filter(lambda x:x>20, lst)
+for item in itemL:
+    print(item)
